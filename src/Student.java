336e66ca2a7446f222407c50f7175b1cd9810fa3
danielle-w233 <dwhitfi@uwo.ca>
2017-02-14 21:45:00 -0500

    Added class Couse and made changes to Student and Instructor.

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,20 @@
 /**
  * Created by ethan on 2017-02-07.
  */
+
+import java.util.*;
+
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
+    }
+
+    public void addCourse(Course c){
+        courseList.add(c);
     }
 
     public String toString() {
